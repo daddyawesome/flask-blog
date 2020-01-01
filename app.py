4db13b213a9f32afec092e156adb80c35e480596
daddyawesome <sablay296@gmail.com>
2020-01-01 13:54:58 +0000

    dunamic url

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/home/<string:name>')
-def hello(name):
-    return "Hello, " + name
+@app.route('/home/user/<string:name>/posts/<int:id>')
+def hello(name, id):
+    return "Hello, " + name +", your id is: " + str(id)
 
 if __name__ == "__main__":
     app.run(debug=True)
