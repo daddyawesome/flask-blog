0b553ce6e2e15b94e86ded687d7e0ab64a9e4b58
daddyawesome <sablay296@gmail.com>
2020-01-01 13:50:46 +0000

    use of veriables on links

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World"
+@app.route('/home/<string:name>')
+def hello(name):
+    return "Hello, " + name
 
 if __name__ == "__main__":
     app.run(debug=True)
