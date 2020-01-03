51a6ed46d5753a3371e6373759faf5417d11da5b
daddyawesome <sablay296@gmail.com>
2020-01-03 14:18:07 +0000

    edit and delete is done. basic crud

--- app.py
+++ app.py
@@ -44,19 +44,24 @@ def posts():
 def delete(id):
     post =BlogPost.query.get_or_404(id)
     db.session.delete(post)
-    db.session.commit
+    db.session.commit()
     return redirect('/posts')
 
 
 @app.route('/posts/edit/<int:id>', methods=['GET','POST'])
 def edit(id):
     post =BlogPost.query.get_or_404(id)
-    post.title = request.form['title']
-    post.author = request.form['author']
-    post.content = request.form['content']
-    db.session.commit()
 
-    return redirect('/posts')
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post=post)
+
+
 
 
 @app.route('/home/user/<string:name>/posts/<int:id>')
